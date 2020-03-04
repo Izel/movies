f30a36c031f09ac7d9afbfd77fa5033fcc741b6b
Izel <gpmeneses@gmail.com>
2020-03-04 21:03:48 +0100

    Code refactor and improving documentation.

--- src/main/scala/com/movies/spark/dataframe/Main.scala
+++ src/main/scala/com/movies/spark/dataframe/Main.scala
@@ -1,24 +1,35 @@
 package com.movies.spark.dataframe
 
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object Main extends App {
+
+  //Spark session for the app
   val spkSession = SparkSession
     .builder()
     .config("spark.master", "local")
     .appName("MoviesRanking")
     .getOrCreate()
 
-  val data = MoviesDataFrame.filterMoviesByYear(1989, MoviesDataFrame.loadMovies(spkSession,"data/movies.dat"))
-  //data.show(10)
+  // Loading movies and Obtaining the released ones after 1989
+  val data = MoviesDataFrame
+    .filterMoviesByYear(1989,
+      MoviesDataFrame.loadMovies(spkSession,"data/movies.dat")
+    )
 
-  val usersDf = MoviesDataFrame.loadUsers(spkSession,"data/users.dat")
-  //usersDf.show(10)
+  // Loading the users but just ones with ages [18,49]
+  val usersDf = MoviesDataFrame.
+    filterUsersByAge(
+      MoviesDataFrame.loadUsers(spkSession,"data/users.dat")
+  )
 
-  val ratingsDf = MoviesDataFrame.loadRatings(spkSession,"data/ratings.dat")
-  //ratingsDf.show(10)
+  // Loading the movies rantings
+  val ratingsDf = MoviesDataFrame.
+    loadRatings(spkSession,"data/ratings.dat")
 
-  val rankings = MoviesDataFrame.moviesRankingByGender(data,usersDf, ratingsDf)
-  rankings.show(10)
+  // Obtaining the rankings
+  val rankings = MoviesDataFrame
+    .moviesRankingByGender(data,usersDf, ratingsDf)
+
+  rankings.show()
 }
--- src/main/scala/com/movies/spark/dataframe/MoviesDataFrame.scala
+++ src/main/scala/com/movies/spark/dataframe/MoviesDataFrame.scala
@@ -45,112 +45,111 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object MoviesDataFrame {
 
   /**
-   * Loads the .dat files into a dataframe without any preprocessing
-   * @param spkSession
+   * Loads the .dat files into a DataFrame without any preprocessing
+   * @param spkSession Spark session to be used for this App
    * @param pathToFile Path to .dat file
-   * @return DataFrame
+   * @return DataFrame with the .dat file content.
    */
   def loadDatFile(spkSession:SparkSession, pathToFile: String): DataFrame = {
-    val df = spkSession.read.option("sep", ":")
+    spkSession.read.option("sep", ":")
       .option("inferSchema", "true")
       .option("header", "false")
       .csv(pathToFile)
-    df
   }
 
+
   /**
-   * Loads and prepares the movies data into a dataframe. Preprocess the movie
+   * Loads and prepares the movies data into a DataFrame. Preprocess the movie
    * release year in a new column.
-   * @param spkSession
+   * @param spkSession Spark session to be used for this App
    * @param pathToFile path to movies.dat
    * @return Dataframe [id, name, categories, year]
    */
   def loadMovies(spkSession:SparkSession, pathToFile: String): DataFrame = {
     val colNames = Seq("MovieID", "null1", "Title", "null2", "Genres")
-      val df = loadDatFile(spkSession,pathToFile)
+    val df = loadDatFile(spkSession,pathToFile)
       .toDF(colNames: _*)
       .drop("null1", "null2")
 
     //Extracts the year from the movie name and set it in a new column for future use
-    val moviesDf = df.withColumn("Year", regexp_extract(col("Title"),"(\\d{4})"
+    df.withColumn("Year", regexp_extract(col("Title"),"(\\d{4})"
       ,1))
-
-    moviesDf
   }
+
 
   /**
    * Process and loads the users data in a DataFrame. Just keep the users with
-   * genders: Female, Male, Trans, Inter.
-   * @param spkSession
-   * @param pathToFile
-   * @return
+   * genders: Female, Male
+   * @param spkSession Spark session to be used for this App
+   * @param pathToFile Path to .dat file
+   * @return DataFrame [UserID, Gender, Age, Occupation, Zip-code]
    */
   def loadUsers(spkSession:SparkSession, pathToFile: String): DataFrame = {
     val colNames = Seq("UserID", "null1", "Gender", "null2",  "Age", "null3",
         "Occupation", "null4", "Zip-code")
     val genders= Seq("M", "F")
-    val usersDf = loadDatFile(spkSession,pathToFile)
+
+    loadDatFile(spkSession,pathToFile)
       .toDF(colNames:_*)
       .drop("null1","null2", "null3", "null4")
       // Keep records with genders in the genders Sequence
       .where(col("Gender").isin(genders:_*))
-    usersDf
   }
 
+
   /**
-   *
-   * @param spkSession
-   * @param pathToFile
-   * @return
+   * Process and loads the ratings data in a DataFrame. Just keeps the ratings
+   * between 0 and 5 [0,5]
+   * @param spkSession Spark session to be used for this App
+   * @param pathToFile Path to .dat file
+   * @return DataFrame [UserID, MovieID, Rating, Timestamp]
    */
   def loadRatings(spkSession:SparkSession, pathToFile: String): DataFrame = {
     val colNames = Seq("UserID", "null1", "MovieID", "null2", "Rating",
       "null3", "Timestamp")
-    val ratingsDF = loadDatFile(spkSession,pathToFile)
+
+    loadDatFile(spkSession,pathToFile)
       .toDF(colNames:_*)
       .drop("null1","null2", "null3")
       .filter("rating <= 5")
-
-    ratingsDF
   }
+
 
   /**
    * Filter movies released before an specific year
-   * @param year
-   * @param df
-   * @return
+   * @param year The year to filter
+   * @param df The DataFrame to filter
+   * @return DataFrame that contains movies released after the specified year.
    */
   def filterMoviesByYear(year:Int, df: DataFrame): DataFrame = {
-    val moviesDf = df.filter(df("year") > year).toDF()
-    moviesDf
+    df.filter(df("year") > year).toDF()
   }
+
 
   /**
    * Filter the users
-   * @param df
+   * @param df The DataFrame to filter
    * @return DataFrame with users ages between 18 and 49 (inclusive)
    */
-  def filterUsersByAge(df: DataFrame) : DataFrame = {
-    val usersDF = df.filter(df("age") > 17 and df("age") < 50).toDF()
-    usersDF
+  def filterUsersByAge(df: DataFrame ): DataFrame = {
+   df.filter(df("age") > 17 and df("age") < 50).toDF()
   }
+
 
   /**
    * Obtain the average range per gender and year.
-   * @param moviesDf
-   * @param usersDf
-   * @param ratingsDf
-   * @return
+   * @param moviesDf The movies DataFrame.
+   * @param usersDf  The users DataFrame.
+   * @param ratingsDf The ratings DataFrame.
+   * @return DataFrame with movies and average rates by gender.
    */
   def moviesRankingByGender(moviesDf:DataFrame, usersDf:DataFrame, ratingsDf:DataFrame): DataFrame = {
-    val rankDf = ratingsDf
+    ratingsDf
       .join(usersDf, "UserID")
       .join(moviesDf, "MovieID")
       .groupBy("MovieID", "Title", "Year", "Gender")
       .agg(avg(ratingsDf.col("rating")).alias("Avg_rating"))//, usersDf.col("Gender"))
       .orderBy("MovieID")
-
-    rankDf
   }
 
 }
