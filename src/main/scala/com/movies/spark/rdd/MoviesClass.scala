9a625af3d2dac8e94d5b0db323a53787662d1a8a
Izel <gpmeneses@gmail.com>
2020-06-04 14:03:39 +0200

    :bulb: Documenting functionalities, objects and classes

--- src/main/scala/com/movies/spark/rdd/MoviesClass.scala
+++ src/main/scala/com/movies/spark/rdd/MoviesClass.scala
@@ -1,13 +1,40 @@
 package com.movies.spark.rdd
 
+/**
+  * Representation of a Movie.
+  * @see [[/MoviesRDD.scala]]
+  * @constructor Create a new movie.
+  * @param id movie ID
+  * @param name Movie Name
+  * @param genres Genres
+  */
 case class Movie(val id: Int, val name: String, val genres: Seq[String])
 
+/**
+  * Representation of an User.
+  * @see [[/MoviesRDD.scala]]
+  * @consrcutor Create a user.
+  * @param id user id
+  * @param gender user gender
+  * @param age user age
+  * @param ocuppation user occupation
+  * @param zipCode user zipcode
+  */
 case class User(val id: Int,
                 val gender: String,
                 val age: Int,
                 val ocuppation: String,
                 val zipCode: String)
 
+/**
+  * Class Rating. Represents an instance of a rating provided by an user to a movie.
+  * @see [[/MoviesRDD.scala]]
+  * @constructor Create a rating
+  * @param userId ID of the user who provide the rating
+  * @param movieId ID of the movie rated by the user
+  * @param rating rate provided by the user to the movie
+  * @param time timestamp of the rate.
+  */
 case class Rating(val userId: Int,
                   val movieId: Int,
                   val rating: Int,
--- src/main/scala/com/movies/spark/rdd/MoviesRDD.scala
+++ src/main/scala/com/movies/spark/rdd/MoviesRDD.scala
@@ -2,7 +2,48 @@ package com.movies.spark.rdd
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  *  The Spark RDD documentation is available on:
+  *  https://spark.apache.org/docs/latest/rdd-programming-guide.html
+  *
+  *  In this exercise, we will load three files in spark RDD. The with the content of three
+  *  .dat files are available in https://grouplens.org/datasets/movielens/1m/
+  *
+  *  We want to:
+  *  - Obtain the average range per gender Vs year and genre Vs year.
+  *
+  *  Constraints:
+  *  - Just keep in count users with age between 18 and 49 years old.
+  *  - Just calculate the rankings for movies released after 1985.
+  *
+  *  The Dataset files don't have header and the fields separator is a double
+  *  character "::"
+  *
+  *  Movies dataset sample:
+  *    MovieID::Title::Genres
+  *    2261::One Crazy Summer (1986)::Comedy
+  *    2262::About Last Night... (1986)::Comedy|Drama|Romance
+  *
+  *  Users Dataset sample:*
+  *    UserID::Gender::Age::Occupation::Zip-code
+  *    25::M::18::4::01609
+  *    26::M::25::7::23112
+  *
+  *  Rankings dataset sample:
+  *    UserID::MovieID::Rating::Timestamp
+  *    1::48::5::978824351
+  *    1::1097::4::978301953
+  *
+  *  For more details about the dataset, check the README file available in the
+  *  dataset location
+  */
 object MoviesRDD {
+
+  /**
+    * Load the movies file in a spark RDD
+    * @param spkCtx spark context
+    * @param pathToFile path to the movies.dat file
+    */
   def loadMoviesFile(spkCtx: SparkContext, pathToFile: String): Unit = {
     val lines = spkCtx.textFile(pathToFile)
     val data = lines.flatMap { line =>
@@ -16,6 +57,11 @@ object MoviesRDD {
     println(data.count())
   }
 
+  /**
+    * Load the user file in a spark RDD
+    * @param spkCtx Spark context
+    * @param pathToFile Path tho users.dat file
+    */
   def loadUsersFile(spkCtx: SparkContext, pathToFile: String): Unit = {
     val lines = spkCtx.textFile(pathToFile)
     val data = lines.flatMap { line =>
@@ -26,6 +72,11 @@ object MoviesRDD {
     println(data.count())
   }
 
+  /**
+    * Load the ratings file in a spark RDD
+    * @param spkCtx spark context
+    * @param pathToFile path to ratings.dat file
+    */
   def loadRatingsFile(spkCtx: SparkContext, pathToFile: String): Unit = {
     val lines = spkCtx.textFile(pathToFile)
     val data = lines.flatMap { line =>
@@ -35,4 +86,5 @@ object MoviesRDD {
     data.take(5) foreach println
     println(data.count())
   }
+
 }
